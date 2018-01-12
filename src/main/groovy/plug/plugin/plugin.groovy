76acf51f38c95977148e8c565fe135f60c810b73
Jean-Charles <jeancharles.roger@me.com>
2018-01-12 13:21:05 +0100

    First import

---
+++ src/main/groovy/plug/plugin/plugin.groovy
@@ -0,0 +1,83 @@
+package plug.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.compile.JavaCompile
+/**
+ */
+class PlugExtension {
+    String version = "0.2"
+}
+
+
+/**
+ * Gradle build plugin for Plug projects
+ *
+ * Created by charlie on 12/01/2018.
+ */
+class PlugBuild implements Plugin<Project> {
+
+    void apply(Project project) {
+
+        project.extensions.create("plug", PlugExtension)
+
+        project.allprojects {
+            apply plugin: 'java'
+            apply plugin: 'maven-publish'
+
+            sourceSets {
+                main {
+                    java { srcDir  'src' }
+                    resources { srcDir 'resources'}
+                }
+                test {
+                    java { srcDir 'tests/src' }
+                    resources { srcDir 'tests/resources'}
+                }
+            }
+
+            // Sets Java compile option to use UTF-8 encoding
+            compileJava.options.encoding = 'UTF-8'
+            tasks.withType(JavaCompile) {
+                options.encoding = 'UTF-8'
+            }
+
+            // Declares repositories to refer to
+            repositories {
+                mavenCentral()
+                maven { url "http://mocs-artefacts.ensta-bretagne.fr/plug-repo/"}
+
+                maven { url = 'http://repository.ops4j.org/maven2/' }
+
+                //needed by javabdd
+                maven { url "https://breda.informatik.uni-mannheim.de/nexus/content/repositories/public" }
+
+                flatDir {
+                    dirs rootProject.file('../external-libs/obp/OBP-1.5.1_batch/jars')
+                }
+            }
+
+            // Alls tests depends on junit 4
+            dependencies {
+                testCompile group: 'junit', name: 'junit', version: '4.+'
+            }
+
+            publishing {
+                publications {
+                    mavenJava(MavenPublication) {
+                        from components.java
+                    }
+                }
+            }
+
+        }
+
+        project.allprojects {
+            group='fr.ensta-bretagne'
+            version = project.plug.version
+        }
+
+    }
+}
+
